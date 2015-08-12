4ef4827780fafcbde5f1d28860f80eb091e82305
Maxime Coste <frrrwww@gmail.com>
2015-08-12 21:51:22 +0100

    Handle quotes in cmd correctly

--- golf.sh
+++ golf.sh
@@ -22,7 +22,7 @@ for challenge in ${challenges}; do
         fi
 
         cp in test
-        kak test -u -n -e "map global user q :wq<ret>; exec '$(<cmd)'; exec i 'did not quit' <esc>; wq"
+        kak test -u -n -e "map global user q :wq<ret>; exec '$(cat cmd | sed -e s/\'/\\\\\'/g)'; exec i 'did not quit' <esc>; wq"
         key_count=$(count_keys "$(<cmd)")
         if cmp -s out test; then
            echo "Challenge ${challenge_url} OK ($key_count keys)"
