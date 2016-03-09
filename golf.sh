216071c897681b42eb2c2d83be5f558270fab2eb
Maxime Coste <frrrwww@gmail.com>
2016-03-09 01:10:14 +0000

    Colorize golf output and compare to vim best score when available

--- golf.sh
+++ golf.sh
@@ -5,6 +5,10 @@ count_keys() {
     echo ${#str}
 }
 
+color_seq() {
+    printf '\033[00;%sm' "$1"
+}
+
 if [ $# -eq 0 ]; then
     challenges=*
 else
@@ -16,7 +20,7 @@ for challenge in ${challenges}; do
         challenge_url="http://vimgolf.com/challenges/${challenge}"
         cd ${challenge}
         if [[ !( -f in && -f out && -f cmd ) ]]; then
-           echo "Challenge ${challenge_url} FAILED (in, out or cmd file missing)"
+           echo "${challenge_url} $(color_seq 31)FAIL$(color_seq 33) (in, out or cmd file missing)$(color_seq 00)"
            cd ..
            continue
         fi
@@ -24,14 +28,24 @@ for challenge in ${challenges}; do
         cp in test
         kak test -u -n -e "map global user q :wq<ret>; exec '$(cat cmd | sed -e s/\'/\\\\\'/g)'; exec i 'did not quit' <esc>; wq"
         key_count=$(count_keys "$(<cmd)")
-        if [ -e vgscore ]; then
-           vim_score=", vim $(cat vgscore))"
+        if [ -f vgscore ]; then
+           vim_score=$(cat vgscore)
+           vim_text=", vim $vim_score)"
+        else
+           vim_score=10000
+           vim_text=""
         fi
         if cmp -s out test; then
-           echo "Challenge ${challenge_url} OK ($key_count keys$vim_score)"
+            tag=OK
+            color=32
+            if [ "$vim_score" -lt $key_count ]; then
+               color=35
+            fi
         else
-           echo "Challenge ${challenge_url} FAIL ($key_count keys$vim_score)"
+            tag=FAIL
+            color=31
         fi
+        echo "${challenge_url} $(color_seq $color)$tag$(color_seq 33) ($key_count keys$vim_text)$(color_seq 00)"
         cd ..
     fi
 done
