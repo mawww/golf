b67101889505201a9e69cb6eaae026cf236d6a13
Maxime Coste <mawww@kakoune.org>
2016-12-12 20:31:28 +0000

    Change vgscore.sh so that it just downloads every scores by default

--- vgscore.sh
+++ vgscore.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "usage: $0 <challenge id>"
-    exit -1
+if [ $# -eq 0 ]; then
+    challenges=*
+else
+    challenges="$@"
 fi
 
-challenge=${1}
-
-curl -s --header 'Accept: text/html,application/xhtml' -L "http://www.vimgolf.com/challenges/${challenge}" |
-    kak -f ':set buffer eolformat lf<ret>/<lt>h5<gt>Lead<ret>/<lt>b<gt><ret>ey%R' > ${challenge}/vgscore
+for challenge in ${challenges}; do
+    if [[ -d ${challenge} ]]; then
+        echo "Reading vimgolf.com best score for ${challenge}"
+        curl -s --header 'Accept: text/html,application/xhtml' \
+             -L "http://www.vimgolf.com/challenges/${challenge}" |
+            kak -f ':set buffer eolformat lf<ret>/<lt>h5<gt>Lead<ret>/<lt>b<gt><ret>ey%R' > ${challenge}/vgscore
+    fi
+done
