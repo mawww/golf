a7809d28ecb11da6cf8ec045c5a72376a6719e42
Jason Felice <jason.m.felice@gmail.com>
2018-09-26 11:26:09 +0100

    Put pipes on end-of-line

--- format-problem-post.sh
+++ format-problem-post.sh
@@ -2,11 +2,11 @@
 
 url="http://www.vimgolf.com/challenges/$1"
 
-curl -s -L -H 'Accept: text/html' "${url}" \
-  | kak -f '/<lt>h3<gt><ret>?<lt>h4<gt><ret>y%R' \
-  | kak -f 's<lt>h5<gt>(.*?)<lt>/h5<gt><ret><esc>c<c-r>1:<esc>' \
-  | kak -f 's<lt>pre[^<gt>]*<gt><ret>c```<ret><esc>' \
-  | kak -f 's<lt>/pre<gt><ret>c```<ret><esc>' \
-  | kak -f 's<lt>[^<gt>]*<gt><ret>d' \
-  | kak -f 'gki**<esc>A**<esc><a-o>2j<a-o>' \
-  | kak -f 'O'"${url}"'<ret><esc>'
+curl -s -L -H 'Accept: text/html' "${url}" |
+  kak -f '/<lt>h3<gt><ret>?<lt>h4<gt><ret>y%R' |
+  kak -f 's<lt>h5<gt>(.*?)<lt>/h5<gt><ret><esc>c<c-r>1:<esc>' |
+  kak -f 's<lt>pre[^<gt>]*<gt><ret>c```<ret><esc>' |
+  kak -f 's<lt>/pre<gt><ret>c```<ret><esc>' |
+  kak -f 's<lt>[^<gt>]*<gt><ret>d' |
+  kak -f 'gki**<esc>A**<esc><a-o>2j<a-o>' |
+  kak -f 'O'"${url}"'<ret><esc>'
